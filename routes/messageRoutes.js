bcf417a49f29c0ed37b1cc32736630dc76beb23d
FordeLuong <liemphongiv666@gmail.com>
2025-08-08 13:47:14 +0700

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/auth'); // Import middleware bảo vệ
+const auth = require('../middleware/auth'); // Import middleware bảo vệ
 
 // Import các hàm controller
 const {
